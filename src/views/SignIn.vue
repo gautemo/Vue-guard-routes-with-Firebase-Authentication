71ba542b7953d76d0509db7b70331d22c6842ae3
Gaute Meek Olsen <gaute.meek@gmail.com>
2020-04-19 14:25:30 +0200

    updated dependencies and simplified SignIn.vue

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -11,22 +11,17 @@ import "firebaseui";
 import "firebaseui/dist/firebaseui.css";
 
 const ui = new firebaseui.auth.AuthUI(firebase.auth());
+const uiConfig = {
+  signInSuccessUrl: "/profile",
+  signInOptions: [
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    firebase.auth.EmailAuthProvider.PROVIDER_ID
+  ]
+};
 
 export default {
   mounted() {
-    this.showSignInUi();
-  },
-  methods: {
-    showSignInUi() {
-      const uiConfig = {
-        signInSuccessUrl: "/profile",
-        signInOptions: [
-          firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-          firebase.auth.EmailAuthProvider.PROVIDER_ID
-        ]
-      };
-      ui.start("#firebaseui-auth-container", uiConfig);
-    }
+    ui.start("#firebaseui-auth-container", uiConfig);
   }
 };
 </script>
